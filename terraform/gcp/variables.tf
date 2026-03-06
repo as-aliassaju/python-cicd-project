cc313d6ee9c71dcbd429544285892a19a9385c00
Alias Saju <aliassaju@evertz.com>
2026-03-06 16:43:59 -0500

    Added GCP Terraform Files"

---
+++ terraform/gcp/variables.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+  description = "ci-cd-pipeline-implementation"
+}
+
+variable "region" {
+  description = "GCP region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  default     = "us-central1-a"
+}
